a99ff48e28ba91f8402c327b544847165c8d710d
DanielAlonsoCasilda <danielcasildaalonso@gmail.com>
2020-07-23 10:45:29 +0200

    inputFields Email y Password fields Datepicker Cambiar el idioma del Datepicker

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/home_temp.dart';
 import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes Flutter',
       debugShowCheckedModeBanner: false,//quitar banner debug
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English, no country code
+        const Locale('es', 'ES'), // Spanish
+        const Locale.fromSubtags(languageCode: 'zh'), // Chinese *See Advanced Locales below*
+        // ... other locales the app supports
+      ],
       //home: HomePage()
       initialRoute: '/',
       //metodo para acceder a rutas
